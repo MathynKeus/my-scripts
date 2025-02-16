a42f6dcdf750da15b06f7483cf3fc15f100e49ad
Mathyn Keus <m.keus@hotmail.com>
2025-02-16 21:21:36 +0100

    Made a hashtable with the Shellmons

--- PokeShell/Pokeshell.ps1
+++ PokeShell/Pokeshell.ps1
@@ -1,1 +1,35 @@
-#Pokeshell - A pokemon game written in Powershell
+#Making a Hashtable with the Shellmons
+$Shellmon = @{
+    "Pikashell" = @{
+        HP = 100
+        Attack = @{
+            Name = "Thundershock"
+            Damage = (Get-Random -Minimum 1 -Maximum 10)
+        }
+    }
+    "Shellizard" = @{
+        HP = 100
+        Attack = @{
+            Name = "Flamethrower"
+            Damage = (Get-Random -Minimum 1 -Maximum 15)
+        }
+    }
+    "Blastoshell" = @{
+        HP = 100
+        Attack = @{
+            Name = "Hydro Pump"
+            Damage = (Get-Random -Minimum 1 -Maximum 15)
+        }
+    }
+    "Venushell" = @{
+        HP = 100
+        Attack = @{
+            Name = "Solar Beam"
+            Damage = (Get-Random -Minimum 1 -Maximum 15)
+        }
+    }
+}
+
+
+
+
