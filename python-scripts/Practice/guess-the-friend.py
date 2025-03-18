c9d170a5a3682be4351054d1e232d644cc277aed
Mathyn Keus <m.keus@hotmail.com>
2025-03-18 21:30:39 +0100

    New practice added

---
+++ python-scripts/Practice/guess-the-friend.py
@@ -0,0 +1,17 @@
+#practice 1.0 (generated with Chat-GPT)
+"""
+	1.	Create a list of five friend names.
+	2.	Ask the user to guess a friend’s name.
+	3.	Check if the guessed name is in the list.
+	4.	If the guess is correct, print "Correct! <name> is my friend."
+	5.	If the guess is wrong, print "Sorry, <name> is not on the list."
+	6.	Make sure the check works even if the user types in lowercase/uppercase variations.
+"""
+#answer
+friends = ["Swiper", "Boots", "Dora", "Diego", "Jeff"]
+guess = input("Guess the friend(case sensitive):")
+if guess in friends: 
+    print("Correct! " + guess + " is my friend.")
+else:
+     print("Sorry,  " + guess + " is not on the list.")
+
