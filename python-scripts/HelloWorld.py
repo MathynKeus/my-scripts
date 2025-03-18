90bd37fcf44a016182f7fe3916a81fd945b62e4a
Mathyn Keus <m.keus@hotmail.com>
2025-03-18 14:00:01 +0100

    Updated

--- python-scripts/HelloWorld.py
+++ python-scripts/HelloWorld.py
@@ -1,4 +1,22 @@
-x = 5
-y = "Mathyn"
+#variables (string)
+character_name = "Mathyn"
+print("The name is: " + character_name)
+character_age = "18" 
+print("The age is :" + character_age)
 
-print(x, y)
+#variables (string + integer)
+character_name = "Mathyn"
+print("The name is: " + character_name)
+character_age = 18 
+print("The age is :" + str(character_age))
+
+#boolean
+is_male = True
+is_female = False
+
+#newline 
+print("Hello,\nWorld!")
+
+#printliterally 
+print("hello\"World")
+
