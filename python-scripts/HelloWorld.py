da548864e6f7adb5b98522348071bd6956694a48
Mathyn Keus <m.keus@hotmail.com>
2025-03-28 11:26:07 +0100

    Updated

--- python-scripts/HelloWorld.py
+++ python-scripts/HelloWorld.py
@@ -119,3 +119,174 @@ def say_hi(name, age):
 
 say_hi("Mike", 33)
 
+#return_statement
+def cube(num):
+    return num*num*num
+
+result = cube(3)
+print(result)
+
+#if_statements
+is_male = True
+is_tall = True
+
+if is_male:
+    print("You are a male")
+else: 
+    print("You are a female")
+
+#or statement
+is_male = False
+is_tall = False
+
+if is_male or is_tall:
+    print("You are a male")
+else: 
+    print("You are a female")
+
+#and statement
+is_male = True
+is_tall = False
+
+if is_male and is_tall:
+    print("You are a male")
+else: 
+    print("You are a female")
+
+#else_if + not_statement
+is_male = True
+is_tall = False
+
+if is_male and is_tall:
+    print("You are a male")
+elif is_male and not (is_tall):
+    print("You are a short male")
+elif not is_male and is_tall:
+    print("You are a short male")
+else: 
+    print("You are a female")
+
+#functions_and_parameters
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(3,15,40))
+
+#building_a_better_calculator
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid operator")
+
+#dictionaries
+monthConversions = {
+    "Jan": "January",
+    1: "February",
+    "Mar": "March",
+}
+
+print(monthConversions["Jan"])
+print(monthConversions.get(1))
+print(monthConversions.get("Jeff", "Not a valid key"))
+
+#while_loop
+i = 1
+while i <= 10:
+    print(i) 
+    i = i + 1
+
+print("Done with loop")
+
+#guessing_game
+secret_word = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess:")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("You lose the game")
+else:
+    print("You win the game")
+
+#for_loop
+for letter in "Jeff":
+    print(letter)
+
+friends = ["Jeff", "Diego", "Swiper"]
+for friend in friends:
+    print(friend)
+
+friends = ["Jeff", "Diego", "Swiper"]
+for index in range(10):
+    print(index)
+
+friends = ["Jeff", "Diego", "Swiper"]
+for index in range(3, 10):
+    print(index)
+
+#exponent_function
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(3,4))
+
+#2D_lists 
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+print(number_grid[1][1])
+
+#nested_loops
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+for row in number_grid:
+    for col in row:
+        print(col)
+
+#building_a_translator
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+            
+    return translation
+
+print(translate(input("Enter a phrase: ")))
